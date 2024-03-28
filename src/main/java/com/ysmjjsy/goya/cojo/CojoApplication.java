c2fc871bca0c5c9f9a9cc0dc11524fc3b0fdc624
goya <77394960@qq.com>
2024-03-28 21:06:00 +0800

    init

--- src/main/java/com/ysmjjsy/goya/cojo/CojoApplication.java
+++ src/main/java/com/ysmjjsy/goya/cojo/CojoApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class CojoApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(CojoApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(CojoApplication.class, args);
+    }
 
 }
--- src/test/java/com/ysmjjsy/goya/cojo/CojoApplicationTests.java
+++ src/test/java/com/ysmjjsy/goya/cojo/CojoApplicationTests.java
@@ -6,8 +6,8 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 class CojoApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @Test
+    void contextLoads() {
+    }
 
 }
