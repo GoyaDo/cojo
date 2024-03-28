45d6c463afec467e04449b96567d3a33368a0b93
goya <77394960@qq.com>
2024-03-28 21:05:13 +0800

    Pleas enter the commit message for your changes. Lines starting

---
+++ src/main/java/com/ysmjjsy/goya/cojo/CojoApplication.java
@@ -0,0 +1,13 @@
+package com.ysmjjsy.goya.cojo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CojoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CojoApplication.class, args);
+	}
+
+}
